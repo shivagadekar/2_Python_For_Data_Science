80197539d56e36220de8f80a94516cbf28a0fc97
Shivam <shivamgadekar1998@gmail.com>
2022-03-07 21:06:18 +0530

    First Lecture Completed and Learner reading csv, txt and excel data

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-import os
+# import os
 import pandas as pd
+
+
+data_csv = pd.read_csv('stock_data.csv', index_col=0, na_values=[2])
+# print(data_csv)
+
+data_xlsx = pd.read_excel('stock_data_xlsx.xlsx', sheet_name='stock_data', index_col=0, na_values=[-11.57])
+# print(data_xlsx)
+
+data_txt_table = pd.read_table('stock_data_txt.txt', index_col=0)
+print(data_txt_table)
+
+data_txt_table_txt = pd.read_csv('stock_data_txt.txt', delimiter=',')
+print(data_txt_table_txt)
